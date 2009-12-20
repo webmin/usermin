6aacd34f1c49d36bba27769a79ff2fbe14688d7f
Jamie Cameron <jcameron@webmin.com>
2009-12-20 10:54:53 -0800

    Module push script updates

--- modules-release.sh
+++ modules-release.sh
@@ -2,7 +2,7 @@
 # Copy all modules to download.webmin.com
 
 fromdir=/usr/local/useradmin
-todir=joe.webmin.com:htdocs/download
+todir=webadmin@download.webmin.com:domains/download.webmin.com/public_html/download
 rsync="rsync --rsh=ssh -v"
 
 $rsync $fromdir/umodules/*.wbm.gz $todir/umodules

f79e7914910b94cb718e1c495ccf9cb07f0fca75
Jamie Cameron <jcameron@webmin.com>
2014-02-09 16:33:34 -0800

    Only consider email addresses with an @ in them http://virtualmin.com/node/32323

--- mailbox/mailbox-lib.pl
+++ mailbox/mailbox-lib.pl
@@ -1333,13 +1333,13 @@ if ($config{'from_map'} && $remote_user !~ /\@/) {
 	while(<MAP>) {
 		s/\r|\n//g;
 		s/#.*$//;
-		if (/^\s*(\S+)\s+(\S+)/ &&
+		if (/^\s*(\S+)\s+(\S+\@\S+)/ &&
 		    ($1 eq $remote_user || &indexof($1, @froms) >= 0) &&
 		    $config{'from_format'} == 0) {
 			# Username on LHS matches
 			push(@mfroms, $2);
 			}
-		elsif (/^\s*(\S+)\s+(\S+)/ &&
+		elsif (/^\s*(\S+\@\S+)\s+(\S+)/ &&
 		       ($2 eq $remote_user || &indexof($2, @froms) >= 0) &&
 		       $config{'from_format'} == 1) {
 			# Username on RHS matches

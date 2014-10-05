aac9c629e66e7706bd4510a8df25106234c29ad6
Jamie Cameron <jcameron@webmin.com>
2014-10-04 22:39:18 -0700

    Polish translations, thanks for Piotr Kozica

---
+++ htaccess/uconfig.info.pl
@@ -0,0 +1,2 @@
+www_root=Katalog domowy (root) dla web,0
+show_names=Wy¶wietlaj nazwy dyrektyw Apache,1,1-Tak,0-Nie
---
+++ telnet/uconfig.info.pl
@@ -0,0 +1,4 @@
+sizemode=Rozmiar apletu,1,0-80x24 znaków (w domy¶lnej czcionce),1-Maksymalny,2-Niestandardowy
+size=Niestandardowy rozmiar &#45; szeroko¶æ x wysoko¶æ,0
+fontsize=Rozmiar czcionki w punktach,3,Domy¶lnie
+detach=Wy¶wietlaj w osobnym oknie,1,1-Tak,0-Nie

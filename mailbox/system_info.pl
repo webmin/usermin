5250f491a708d53e8de0ec8482495bdbbb671f9e
Jamie Cameron <jcameron@webmin.com>
2014-12-09 17:27:11 -0800

    System info provider with general host details

---
+++ mailbox/system_info.pl
@@ -0,0 +1,37 @@
+
+use strict;
+use warnings;
+require 'mailbox-lib.pl';
+our (%text, %gconfig);
+
+# list_system_info()
+# Return some basic info about the system
+sub list_system_info
+{
+my @table;
+my $table = { 'type' => 'table',
+              'desc' => $text{'right_header'},
+              'table' => \@table };
+
+# Hostname
+push(@table, { 'desc' => $text{'right_host'},
+	       'value' => &get_display_hostname() });
+
+# Operating system
+push(@table, { 'desc' => $text{'right_os'},
+               'value' => $gconfig{'os_version'} eq '*' ?
+                        $gconfig{'real_os_type'} :
+                        $gconfig{'real_os_type'}.' '.$gconfig{'real_os_version'}
+             });
+
+# Usermin version
+push(@table, { 'desc' => $text{'right_usermin'},
+	       'value' => &get_webmin_version() });
+
+# System time
+my $tm = localtime(time());
+push(@table, { 'desc' => $text{'right_time'},
+	       'value' => $tm });
+
+return ($table);
+}

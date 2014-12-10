2bba1c89c4a5f61f96d578aee192cd4e18f0180a
Jamie Cameron <jcameron@webmin.com>
2014-12-09 17:49:13 -0800

    Return quota usage information

---
+++ quota/system_info.pl
@@ -0,0 +1,31 @@
+
+use strict;
+use warnings;
+require 'quota-lib.pl';
+our (%text, %config, $remote_user, %filesys);
+
+# list_system_info()
+# Return some basic info about the system
+sub list_system_info
+{
+my $n = &quota::user_filesystems($remote_user);
+if ($n > 0) {
+	my @chart;
+	my $chart = { 'type' => 'chart',
+		      'desc' => $text{'right_header'},
+		      'chart' => \@chart };
+	for(my $i=0; $i<$n; $i++) {
+		my $quota = $filesys{$i,'hblocks'} ||
+			    $filesys{$i,'sblocks'};
+		my $usage = $filesys{$i,'ublocks'};
+		my $bsize = $config{'block_size'};
+		push(@chart, { 'desc' => $filesys{$i,'filesys'},
+			       'chart' => [ $quota, $usage ],
+			       'value' => &text('right_out',
+					&nice_size($usage*$bsize),
+					&nice_size($quota*$bsize)), });
+		}
+	return ($chart);
+	}
+return ( );
+}

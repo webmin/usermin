a60ff5717a0e8040c88230921092cf5c58588993
Jamie Cameron <jcameron@webmin.com>
2024-12-13 14:22:53 -0800

    Set the temp dir after we've checked return status from cat

--- setup.sh
+++ setup.sh
@@ -15,15 +15,15 @@ fi
 srcdir=$wadir
 ver=`cat "$wadir/version"`
 
-# Find temp directory
-if [ "$tempdir" = "" ]; then
-	tempdir=/tmp/.webmin
-fi
-
 if [ $? != "0" ]; then
 	echo "ERROR: Cannot find the Usermin install directory";
 	echo "";
 	exit 1;
+fi
+
+# Find temp directory
+if [ "$tempdir" = "" ]; then
+	tempdir=/tmp/.webmin
 fi
 
 echo "****************************************************************************"
